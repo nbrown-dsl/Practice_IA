d574453eef370eef833ae53e76cf39ba2f634bd6
CharSB <noahanimatesstuff@gmail.com>
2022-03-18 10:56:16 +0000

    idk a test commit?

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+subscribers = []
 
 @app.route('/')
 def index():
@@ -8,7 +10,7 @@ def index():
 
 @app.route('/about')
 def about():
-    names =["Noah", "Charles", "Anastasia", "Dot"]
+    names =["Noah", "Charles", "Anastasia", "Dot", "Daamitha"]
     return render_template("about.html", names=names)
 
 @app.route('/subscribe')
@@ -18,5 +20,14 @@ def subscribe():
 #only allowed to come here if you have submitted a form to here
 @app.route('/form', methods=["POST"])
 def form():
-    first_name = subscribe
-    return render_template("form.html", first_name=first_name)
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+
+    if not first_name or not last_name or not email:
+        error_statement = "All form fields required..."
+        return render_template("subscribe.html", error_statement=error_statement, first_name=first_name, last_name=last_name, email=email)
+
+    subscribers.append(f"{first_name} {last_name} | {email}")
+
+    return render_template("form.html", subscribers=subscribers)
